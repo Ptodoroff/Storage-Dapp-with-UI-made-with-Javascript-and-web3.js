c26efc1d3f0c0ba53b355685c3dc8797d38c6ff9
Petar Todorov <petartodorov@Petars-MacBook-Pro.local>
2022-08-13 21:49:54 +0300

    finalsied the tests

--- test/advancedstoragetest.js
+++ test/advancedstoragetest.js
@@ -11,18 +11,29 @@ contract ("Advanced Storage", () =>{
     it ("should add the input", async () =>{
         let input =8;
         await storage.add(input);
-        let result = await storage.ids(0);
+        let result = await storage.ids(2);
         assert(result==input, "Accepts the input value")
 
      })
 
-     it ("give return the entire array", async () =>{
-        let test = await storage.getAll();
-        let result =  storage.ids;
-        assert(result==test, "returns the array")
+     it ("should return the entire array", async () =>{
+        let rawIds = await storage.getAll();
+        let result =  rawIds.map(id=> id.toNumber());
+        assert.deepEqual(result,[10,5,8,10,5], "returns the array")
 
+     });
+
+     it ("should return a specific element", async () =>{
+        let input = await storage.get(0)
+        assert(input==10, "the chosen element is returned");
+     })
+
+     it ("should return the array length", async () =>{
+        let length = await storage.arrayLength()
+        assert(length==9, "the chosen element is returned");
+     })
 
      
 })
 
-})
+
