e3b6aabf8a47955e9b6a60779513523d1474a5f3
Petar Todorov <petartodorov@Petars-MacBook-Pro.local>
2022-08-13 22:34:37 +0300

    set up the webpack envoronment and started building the forntend

---
+++ app/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+  ],
+  devServer: { contentBase: path.join(__dirname, "public"), compress: true, port:8080 },
+};
