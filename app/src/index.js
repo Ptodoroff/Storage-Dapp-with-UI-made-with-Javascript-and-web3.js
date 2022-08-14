a0a1436ef7dbbe14ec51768e0b90498729e37f17
Petar Todorov <petartodorov@Petars-MacBook-Pro.local>
2022-08-14 03:08:09 +0300

    finalisation

--- app/src/index.js
+++ app/src/index.js
@@ -1,76 +1,81 @@
-import Web3 from "web3";
-import StorageArtifact from "../../build/contracts/Storage.json";
+import Web3 from'web3';
+import Storage from '../../build/contracts/Storage.json';
 
-const App = {
-  web3: null,
-  account: null,
-  meta: null,
+let web3;
+let storage;
 
-  start: async function() {
-    const { web3 } = this;
 
-    try {
-      // get contract instance
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = StorageArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        StorageArtifact.abi,
-        deployedNetwork.address,
-      );
+const initWeb3 = () => {
+    return new Promise((resolve, reject) => {
+      if(typeof window.ethereum !== 'undefined') {
+        const web3 = new Web3(window.ethereum);
+        window.ethereum.enable()
+          .then(() => {
+            resolve(
+              new Web3(window.ethereum)
+            );
+          })
+          .catch(e => {
+            reject(e);
+          });
+        return;
+      }
+      if(typeof window.web3 !== 'undefined') {
+        return resolve(
+          new Web3(window.web3.currentProvider)
+        );
+      }
+      resolve(new Web3('http://localhost:9545'));
+    });
+  };
+  
 
-      // get accounts
-      const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
 
-      this.refreshBalance();
-    } catch (error) {
-      console.error("Could not connect to contract or chain.");
-    }
-  },
+const initContract =  () => {                                // this is the fn that will initialise the instance of the contract
+    
+    return  new web3.eth.Contract(Storage.abi,Storage.networks[5777].address)
+}
 
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
 
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
-  },
 
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
+const  initApp = () => {                                     //initialises the app
+    let $addData=document.getElementById("addData");
+    let $data = document.getElementById("data");
+    let accounts =[];
 
-    this.setStatus("Initiating transaction... (please wait)");
+    web3.eth.getAccounts()
+    .then(_accounts=>{
+        accounts=_accounts;
+        return Storage.methods.getAll().call()
+    })
+    .then((result)=>{
+        $data.innerHTML= result.join(', ');
+    })
 
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
+    $addData.addEventListener('submit', (event) =>{
+        event.preventDefault();
+        Storage
+        .methods
+        .add(event.target.elements[0].value)
+        .send({from:accounts[0]})
+        .then( ()=> {
+            return Storage.methods
+            .getAll()
+            .call()
+        })
 
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
-  },
+    })
+    
+    
 
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-};
+}
 
-window.App = App;
-
-window.addEventListener("load", function() {
-  if (window.ethereum) {
-    // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    window.ethereum.enable(); // get permission to access accounts
-  } else {
-    console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
-    );
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    );
-  }
-
-  App.start();
-});
+document.addEventListener('DOMContentLoaded', () =>{
+    initWeb3()
+    .then(_web3 => {
+        web3=_web3;
+        storage = initContract();
+        initApp();
+    })
+    .catch((e)=>console.log(e.message))
+})
